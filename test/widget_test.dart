a1a75bae486294738dfb7681b93b17686e5928a6
Abilkhair Seilov <abilkhairseilov@outlook.com>
2025-01-19 22:03:53 +0300

    restart app :facepalm:

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_project/main.dart';
+import 'package:lifelines_25/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
